17a2eb3dcfdaa23e30753e749a06b2a2f042ec44
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-11-22 13:54:59 +0100

    Set UART divisor to make Linux' earlycon work.

--- machine/uart.c
+++ machine/uart.c
@@ -61,6 +61,13 @@ static void uart_done(const struct fdt_scan_node *node, void *extra)
   uart = (void*)(uintptr_t)scan->reg;
   uart[UART_REG_TXCTRL] = UART_TXEN;
   uart[UART_REG_RXCTRL] = UART_RXEN;
+
+  // set divisor for sifive uart
+  if (scan->compat) {
+    // core runs with 80 MHz and we use 115200 baud
+    uint16_t div = ((80000000 + 115200 - 1) / 115200) - 1;
+    uart[UART_REG_DIV] = div;
+  }
 }
 
 void query_uart(uintptr_t fdt)
